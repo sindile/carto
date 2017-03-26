2183b3deb76620f8318174d3de65a75828adf59f
teanocrata <teanocrata@gmail.com>
2017-03-26 18:53:01 +0200

    Config variables for cartodb and restart  portgres database after test

--- cartodb/run.sh
+++ cartodb/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 # Initialize the metadata database
+sed -i "s^__DB_PORT__^$DB_PORT^g" config/database.yml
+sed -i "s^__DB_HOST__^$DB_HOST^g" config/database.yml
+sed -i "s^__DB_USER__^$DB_USER^g" config/database.yml
+sed -i "s^__DB_PORT__^$DB_PORT^g" config/app_config.yml
+sed -i "s^__DB_HOST__^$DB_HOST^g" config/app_config.yml
+sed -i "s^__DB_USER__^$DB_USER^g" config/app_config.yml
+sed -i "s^__REDIS_HOST__^$REDIS_HOST^g" config/database.yml
+sed -i "s^__REDIS_PORT__^$REDIS_PORT^g" config/database.yml
+sed -i "s^__REDIS_HOST__^$REDIS_HOST^g" config/app_config.yml
+sed -i "s^__REDIS_PORT__^$REDIS_PORT^g" config/app_config.yml
 
-RAILS_ENV=development bundle exec rake db:create && RAILS_ENV=development bundle exec rake db:migrate
+RAILS_ENV=$CARTO_ENV bundle exec rake db:create && RAILS_ENV=$CARTO_ENV bundle exec rake db:migrate
