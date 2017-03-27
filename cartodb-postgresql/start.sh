d8157bcfa376ca4858f2ffb30c8652cfed754711
teanocrata <teanocrata@gmail.com>
2017-03-27 07:53:59 +0200

    Some more environment and init process of postgress in dockerfile instead sh

--- cartodb-postgresql/start.sh
+++ cartodb-postgresql/start.sh
@@ -1,49 +1,12 @@
 #!/bin/bash
 
-service postgresql start
-wait $!
-echo "*************************************************************************"
-echo "Create user public user"
-echo "*************************************************************************"
-createuser publicuser --no-createrole --no-createdb --no-superuser -U postgres
-wait $!
-echo "*************************************************************************"
-echo "Create user tileuser"
-echo "*************************************************************************"
-createuser tileuser --no-createrole --no-createdb --no-superuser -U postgres
-wait $!
-echo "*************************************************************************"
-echo "Create template database"
-echo "*************************************************************************"
-createdb -T template0 -O postgres -U postgres -E UTF8 template_postgis
-wait $!
-echo "*************************************************************************"
-echo "Create land"
-echo "*************************************************************************"
-createlang plpgsql -U postgres -d template_postgis
-wait $!
-echo "*************************************************************************"
-echo "Create extension postgis"
-echo "*************************************************************************"
-psql -U postgres template_postgis -c 'CREATE EXTENSION postgis;CREATE EXTENSION postgis_topology;'
-wait $!
-echo "*************************************************************************"
-echo "ldconfig"
-echo "*************************************************************************"
-ldconfig
-wait $!
 echo "*************************************************************************"
 echo "Change directory"
 echo "*************************************************************************"
 cd cartodb-postgresql
 wait $!
 echo "*************************************************************************"
-echo "Test"
-echo "*************************************************************************"
-PGUSER=postgres make installcheck
-wait $!
-echo "*************************************************************************"
-echo "Restart postgres"
+echo "Start postgres"
 echo "*************************************************************************"
 service postgresql start
 echo "*************************************************************************"
